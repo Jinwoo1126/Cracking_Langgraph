46c140f4b72c1884221d2b0c8547cadb5c8b1eb2
Jinwoo1126 <wdfrty1234@gmail.com>
2025-07-01 16:37:35 +0900

    human-response

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ from typing_extensions import TypedDict
 
 from langgraph.graph import StateGraph, START, END
 from langgraph.graph.message import add_messages
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.prebuilt import ToolNode, tools_condition
+from langgraph.types import Command, interrupt
 
 from langchain_core.tools import tool
 from langchain_core.messages import ToolMessage
@@ -20,19 +23,9 @@ load_dotenv()
 # Set up the API key for Tavily
 os.environ["TAVILY_API_KEY"] = os.getenv("TAVILY_API_KEY")
 
-tool = TavilySearch(max_results=2)
-tools = [tool]
-tool.invoke("What's a 'node' in LangGraph?")
-
 class State(TypedDict):
     # messages have the type "list". and 'add_messages' funciton
     messages: Annotated[list, add_messages]
-
-llm = ChatOllama(model="qwen3")
-llm_with_tools = llm.bind_tools(tools)
-
-def chatbot(state: State):
-    return {"messages": [llm_with_tools.invoke(state["messages"])]}
 
 class BasicToolNode:
     """A node that runs the tools requested in the last AIMessage."""
@@ -76,54 +69,140 @@ def route_tools(
         return "tools"
     return END
 
+@tool
+def human_assistance(query: str) -> str:
+    """Request assistance from a human."""
+    human_response = interrupt({"query": query})
+    return human_response["data"]
+
+tool = TavilySearch(max_results=2)
+tools = [tool, human_assistance]
+
+llm = ChatOllama(model="qwen3")
+llm_with_tools = llm.bind_tools(tools)
+
+def chatbot(state: State):
+    message = llm_with_tools.invoke(state["messages"])
+    # Because we will be interrupting during tool execution,
+    # we disable parallel tool calling to avoid repeating any
+    # tool invocations when we resume.
+    assert len(message.tool_calls) <= 1
+    return {"messages": [message]}
 
 graph_builder = StateGraph(State)
 
 # Add chatbot node
-graph_builder.add_node("chatbot", chatbot)
+# graph_builder.add_node("chatbot", chatbot)
 
-tool_node = BasicToolNode(tools=[tool])
+# tool_node = BasicToolNode(tools=[tool])
+# graph_builder.add_node("tools", tool_node)
+# graph_builder.add_conditional_edges(
+#     "chatbot",
+#     route_tools,
+#     # The following dictionary lets you tell the graph to interpret the condition's outputs as a specific node
+#     # It defaults to the identity function, but if you
+#     # want to use a node named something else apart from "tools",
+#     # You can update the value of the dictionary to something else
+#     # e.g., "tools": "my_tools"
+#     {"tools": "tools", END: END},
+# )
+# # Any time a tool is called, we return to the chatbot to decide the next step
+# graph_builder.add_edge("tools", "chatbot")
+# graph_builder.add_edge(START, "chatbot")
+# graph = graph_builder.compile(checkpointer=memory)
+
+graph_builder.add_node("chatbot", chatbot)
+tool_node = ToolNode(tools=tools)
 graph_builder.add_node("tools", tool_node)
+
 graph_builder.add_conditional_edges(
     "chatbot",
-    route_tools,
-    # The following dictionary lets you tell the graph to interpret the condition's outputs as a specific node
-    # It defaults to the identity function, but if you
-    # want to use a node named something else apart from "tools",
-    # You can update the value of the dictionary to something else
-    # e.g., "tools": "my_tools"
-    {"tools": "tools", END: END},
+    tools_condition,
 )
-# Any time a tool is called, we return to the chatbot to decide the next step
+
 graph_builder.add_edge("tools", "chatbot")
 graph_builder.add_edge(START, "chatbot")
-graph = graph_builder.compile()
+
+memory = MemorySaver()
+graph = graph_builder.compile(checkpointer=memory)
+
+# def stream_graph_updates(user_input: str):
+#     for event in graph.stream({"messages": [{"role": "user", "content": user_input}]}):
+#         for value in event.values():
+#             print("Assistant:", value["messages"][-1].content)
+
+# while True:
+#     try:
+#         user_input = input("User: ")
+#         if user_input.lower() in ["quit", "exit", "stop"]:
+#             print("Goodbye!")
+#             break
+#         stream_graph_updates(user_input)
+#     except:
+#         user_input = "What do you know about LangGraph?"
+#         print("User: " + user_input)
+#         stream_graph_updates(user_input)
+#         break
 
 
-# from IPython.display import Image, display
+### Add memory
+# config = {"configurable": {"thread_id": "1"}}
 
-# try:
-#     display(Image(graph.get_graph().draw_mermaid_png()))
-# except Exception:
-#     # This requires some extra dependencies and is optional
-#     print("Graph visualization is not available. Please install the required dependencies to visualize the graph.")
-#     pass
+# user_input = "Hi there! My name is Will."
+
+# # The config is the **second positional argument** to stream() or invoke()!
+# events = graph.stream(
+#     {"messages": [{"role": "user", "content": user_input}]},
+#     config,
+#     stream_mode="values",
+# )
+# for event in events:
+#     event["messages"][-1].pretty_print()
+
+# user_input = "Remember my name?"
+
+# # The config is the **second positional argument** to stream() or invoke()!
+# events = graph.stream(
+#     {"messages": [{"role": "user", "content": user_input}]},
+#     config,
+#     stream_mode="values",
+# )
+# for event in events:
+#     event["messages"][-1].pretty_print()
+
+# # The only difference is we change the `thread_id` here to "2" instead of "1"
+# events = graph.stream(
+#     {"messages": [{"role": "user", "content": user_input}]},
+#     {"configurable": {"thread_id": "2"}},
+#     stream_mode="values",
+# )
+# for event in events:
+#     event["messages"][-1].pretty_print()
 
 
-def stream_graph_updates(user_input: str):
-    for event in graph.stream({"messages": [{"role": "user", "content": user_input}]}):
-        for value in event.values():
-            print("Assistant:", value["messages"][-1].content)
+user_input = "I need some expert guidance for building an AI agent. Could you request assistance for me?"
+config = {"configurable": {"thread_id": "1"}}
 
-while True:
-    try:
-        user_input = input("User: ")
-        if user_input.lower() in ["quit", "exit", "stop"]:
-            print("Goodbye!")
-            break
-        stream_graph_updates(user_input)
-    except:
-        user_input = "What do you know about LangGraph?"
-        print("User: " + user_input)
-        stream_graph_updates(user_input)
-        break
+events = graph.stream(
+    {"messages": [{"role": "user", "content": user_input}]},
+    config,
+    stream_mode="values",
+)
+for event in events:
+    if "messages" in event:
+        event["messages"][-1].pretty_print()
+
+snapshot = graph.get_state(config)
+print(snapshot.next)
+
+human_response = (
+    "We, the experts are here to help! We'd recommend you check out LangGraph to build your agent."
+    " It's much more reliable and extensible than simple autonomous agents."
+)
+
+human_command = Command(resume={"data": human_response})
+
+events = graph.stream(human_command, config, stream_mode="values")
+for event in events:
+    if "messages" in event:
+        event["messages"][-1].pretty_print()
